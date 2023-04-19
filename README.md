76ba73e1cee2b84af0d0ba007bfee2320f9c8057
Shrey Batra <shrey.batra@cosmocloud.io>
2023-04-19 18:07:52 +0530

    initial comit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Python Task 1
+
+Complete the function in `main.py`, by merging the information from list_1 and list_2
+to create a new list, which has all the information about each student from
+both lists in one single dict.
+
+- Both lists are unsorted
+- Both lists can have missing values (for ex list_2 has missing id=2)
+
+# Submission Guidelines
+
+- You need to submit the task, completed by `11:59PM IST - 21st April, 2023`.
+- You need to push the updated code in a Github repository in your own account and submit the link to the repository in [this form](https://forms.gle/6i5wrfDwr661JXY79).
---
+++ main.py
@@ -0,0 +1,32 @@
+list_1 = [
+    {"id": "1", "name": "Shrey", "age": 25},
+    {"id": "3", "age": 10, "name": "Hello"},
+    {"id": "2", "name": "World", "age": 24},
+]
+
+list_2 = [
+    {"id": "1", "marks": 100},
+    {
+        "id": "3",
+        "marks": 90,
+        "roll_no": 11,
+        "extra_info": {
+            "hello": "world",
+        },
+    },
+]
+
+
+def merge_lists(list_1, list_2) -> list:
+    """
+    Complete this function, by merging the information from list_1 and list_2
+    to create a new list, which has all the information about each student from
+    both lists in one single dict.
+
+    - Both lists are unsorted
+    - Both lists can have missing values (for ex list_2 has missing id=2)
+    """
+    # return list_3
+
+
+list_3 = merge_lists(list_1, list_2)
